a4b60221d4cc32d74e59697891613cda8b447130
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-11-22 13:55:38 -0300

    Create DML.sql

---
+++ DML.sql
@@ -0,0 +1,9 @@
+--INSERT INTO table_name (column1, column2,...)
+
+INSERT INTO Clientes (IDCliente, Nome, Endereco, Telefone)
+VALUES (1, 'Joao', 'rua augusta', '6946954');
+
+-- SELECT coluna1, coluna2,...
+-- FROM nome_da_tabela
+
+SELECT * FROM Clientes;
