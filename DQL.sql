b81c12a8c393c8b911a492975721c687a52edc64
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-11-22 15:26:23 -0300

    Update DQL.sql

--- DQL.sql
+++ DQL.sql
@@ -6,3 +6,12 @@ FROM Clientes;
 SELECT C.Nome AS NomeCliente
   FROM Clientes C JOIN Pedidos P
   ON C.IDCliente = P.IDCliente;
+
+-- COMANDO DISTINCT (pode ser usado para retornar apenas valores distintos (únicos de uma coluna ou combinação de colunas) ou para selecionar valores únicos de uma coluna presente em ambas as tabelas)
+
+SELECT DISTINCT categoria
+  FROM Produtos;
+
+SELECT DISTINCT C.Nome AS NomeCliente
+	FROM Clientes C JOIN Pedidos P
+	ON C.IDCliente = P.IDCliente;
