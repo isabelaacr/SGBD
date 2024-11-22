c74535d0eb1dc932f5937637478c85d6686c99cf
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-11-22 14:04:51 -0300

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -7,3 +7,10 @@ VALUES (1, 'Joao', 'rua augusta', '6946954');
 -- FROM nome_da_tabela
 
 SELECT * FROM Clientes;
+
+INSERT INTO Clientes (IDCliente, Nome, Endereco, Telefone)
+VALUES (2, 'Maria', 'rua anita', '6342954'),
+       (3, 'Jose', 'rua dois', '6322954');  --Não tenho atributos nomeados
+
+INSERT INTO Clientes  --omitindo os atributos, porém seguindo a ordem dos atributos na tabela
+VALUES (4, 'Pedro', 'rua quatro', '45783578');
