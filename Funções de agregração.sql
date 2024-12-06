fd8d006f1b445f3538395eabfeea66a38d01cb26
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-12-06 14:12:56 -0300

    Create Funções de agregração.sql

---
+++ Funções de agregração.sql
@@ -0,0 +1,23 @@
+-- COUNT: usada para contar o número de linhas retornadas por uma consulta.
+
+SELECT COUNT ( ) AS
+  FROM ;
+
+-- SUM: calcula a soma soa calores de uma coluna numérica
+SELECT SUM ( )
+  FROM ;
+
+-- AVG: Calcula a média aritmética de um conjunto de valores contidos em um campo especificado em uma consulta.
+
+-- MAX: retorna o valor máximo de uma coluna
+SELECT MAX ( ) AS
+  FROM ;
+
+-- MIN: retorna o valor mínimo de uma coluna
+SELECT MIN( ) AS
+  FROM ;
+
+-- GROUP BY: agrupar registros que possuem valores iguais
+SELECT 
+  FROM
+  GROUP BY ;
